fb463476d24c2426eaabb21dd9b09db7cd6cff32
Line5 <mail@line5.tv>
2013-09-23 23:38:32 +0200

    release 0.22 - improved WIN XP compatibility
    changed form icon sizes from 64x64 to 48x48 px for compatibility with
    windows XP

--- MessupClientWin/MessupClientWin/Form1.Designer.vb
+++ MessupClientWin/MessupClientWin/Form1.Designer.vb
@@ -101,7 +101,7 @@ Partial Class Form1
         Me.IsMdiContainer = True
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
-        Me.Text = "MESSUP client 0.21"
+        Me.Text = "MESSUP client 0.22"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         Me.StatusStrip1.ResumeLayout(False)
